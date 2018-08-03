96c401dff893a6f936930e091805ae999ab4a917
OR <oreyesc@gmail.com>
2018-08-03 15:36:49 -0500

    API V.5

--- config/config.js
+++ config/config.js
@@ -24,13 +24,8 @@ config.stripe       = {
 
 
 config.mailgun      = {
-<<<<<<< HEAD
 	apiKey  : 'This key can not be shared, use your own key',   // This key can not be shared, use your own key
 	domain  : 'This data can not be shared, use your own domain' // This data can not be shared, use your own domain
-=======
-	apiKey  : 'This key can not be shared, use your own key',
-	domain  : 'This data can not be shared, use your own domain',
->>>>>>> origin/master
 };
 
 
